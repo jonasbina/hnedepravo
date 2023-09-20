1e50337c8368a4752c57b75373b8fd19b240aadb
Jonas <jonysbina@gmail.com>
2023-09-20 18:06:47 +0200

    edits

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 
 
 const Header = () => {
   return (
-    <h1>Hnědé Právo</h1>
-    <img src="/logo.svg" alt="Hnědé Právo" />
+    <div>
+      <Head>
+        <h1>Hnědé Právo</h1>
+        <img src="/logo.svg" alt="Hnědé Právo" />  
+      </Head>
+    </div>
+    
   )
 }
 
