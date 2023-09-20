5bfceba283a7c40aba1bf92832e410c3d2de21ea
Jonas <jonysbina@gmail.com>
2023-09-20 19:27:37 +0200

    edits

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Link from 'next/link'
 const Header = () => {
   return (
     <div>
+      <Link href="/">
+      <a>
         <Image src="/logo.svg" height={700} width={1000} alt="logo" />
+        </a>
+        </Link>
     </div>
     
   )
