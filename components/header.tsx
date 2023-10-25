c3e455354863af328f1ad1429421dadb62bae00f
Jonas <jonysbina@gmail.com>
2023-10-25 16:41:01 +0200

    nova edice

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 const Header = () => {
   return (
-    <div className='content-center'>
+    <div className='w-1/1 content-center'>
       <Link href="/">
       <a>
-        <Image src="/logo.svg" height={700} width={700} alt="logo" />
+        <Image src="/logo.svg" height={700} width={1000} alt="logo" />
         </a>
         </Link>
     </div>
