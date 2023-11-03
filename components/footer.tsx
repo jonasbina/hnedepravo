b5a21fa711f51b4591929371b1e0287ad1bd903a
Jonas <jonysbina@gmail.com>
2023-11-03 22:09:35 +0100

    nova edice

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,20 +5,18 @@ const randomTexts = [
     'Vyčůrej se do mrazáku',
     '"To je tak debilní, miluju to!" - Albert Einstein',
     'Čte to vůbec někdo?',
-    'Vyčůrej se do spacáku.',
     'Pullni ten trigger a bude z tebe n-',
     'Ahoj, je mi Jarda, jmenuji se jednačtyřicet a jsem youtuber!',
-    'Mňau',
     'Dex dex dex -',
-    'Propaganda mňam',
     'Věděli jste, že pokud přijdete o obě nohy, nebudete už moct chodit?',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!!',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!!',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!!',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!!',
-    'Jsem ve tvých zdech *For legal reasons that`s a joke',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
     'Věděli jste, že jste nevěděli?',
-    'Bagr je v podstatě traktor, který umí bagrovat.'
+    'Bagr je v podstatě traktor, který umí bagrovat.',
+    'Nikdo nic neviděl',
+    'Ondřeji prosím!',
   ];
   
   const randomIndex = Math.floor(Math.random() * randomTexts.length);
