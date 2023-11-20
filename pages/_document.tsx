b752c3074d76adb99bbdcf9b3876e0038b917a22
Jonas <jonysbina@gmail.com>
2023-11-20 15:24:51 +0100

    nova edice

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,15 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-          {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
-            // eslint-disable-next-line @next/next/no-sync-scripts
-            <script
-              data-project-id="XVMVkpVRpYeyQa6Gr2yv6jnH7T7WmDBNx8ElZXP2"
-              data-is-production-environment="false"
-              src="https://snippet.meticulous.ai/v1/meticulous.js"
-            />
-          )}
-
+        
       </Head>
       <body>
         <Main />
