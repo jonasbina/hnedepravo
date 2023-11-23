f0897b23a1e0334474ea2fdbd8e2ceb265825962
Jonas <jonysbina@gmail.com>
2023-11-23 20:52:38 +0100

    Added footer to header. Don't ask

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,22 +8,26 @@ const Footer = () => {
     const handleRating = (rate: number) => {
         setRating(rate);
         if (rate < 5) {
-            alert('The selected rating is less than 5!');
+            alert('Ty negře dej tam 5 hvězd!');
         }
     };
 
     return (
-        <footer className="p-4 bg-gray-800 text-white">
-            <div className="flex space-x-2">
-                {[...Array(5)].map((_, i) => (
-                    <button
-                        onClick={() => handleRating(i + 1)}
-                        className="text-2xl cursor-pointer"
-                        key={i}
-                    >
-                        {i + 1 <= rating ? '★' : '☆'}
-                    </button>
-                ))}
+        <footer className="p-4 bg-black text-white flex items-center justify-center">
+            <div className="flex flex-col items-center text-center">
+                <h1 className="mb-4 text-lg">Please rate our website</h1>
+                <div className="flex space-x-2 mb-4">
+                    {[...Array(5)].map((_, i) => (
+                        <button
+                            onClick={() => handleRating(i + 1)}
+                            className="text-2xl cursor-pointer hover:text-yellow-500"
+                            key={i}
+                        >
+                            {i + 1 <= rating ? '★' : '☆'}
+                        </button>
+                    ))}
+                </div>
+                <p className="text-sm">Your feedback is important to us</p>
             </div>
         </footer>
     );
