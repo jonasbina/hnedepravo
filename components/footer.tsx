6320e65e661d1cf14f484e8d14883da48789c399
Jonas <jonysbina@gmail.com>
2023-11-23 20:39:35 +0100

    Added footer to header. Don't ask

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,31 +1,32 @@
 import Container from './container'
 import { EXAMPLE_PATH } from '../lib/constants'
+import { useState } from 'react';
 
-const randomTexts = [
-    'Vyčůrej se do mrazáku',
-    '"To je tak debilní, miluju to!" - Albert Einstein',
-    'Čte to vůbec někdo?',
-    'Pullni ten trigger a bude z tebe n-',
-    'Ahoj, je mi Jarda, jmenuji se jednačtyřicet a jsem youtuber!',
-    'Dex dex dex -',
-    'Věděli jste, že pokud přijdete o obě nohy, nebudete už moct chodit?',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
-    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
-    'Věděli jste, že jste nevěděli?',
-    'Bagr je v podstatě traktor, který umí bagrovat.',
-    'Nikdo nic neviděl',
-    'Ondřeji prosím!',
-  ];
-  
-  const randomIndex = Math.floor(Math.random() * randomTexts.length);
-  const randomText = randomTexts[randomIndex];
-  
-  const Footer = () => (
-    <footer style={{ textAlign: 'center', backgroundColor: 'black', color: 'white', padding: '35px', fontSize: '1.6em' }}>
-      {randomText}
-    </footer>
-  );
-  
-  export default Footer;
+const Footer = () => {
+    const [rating, setRating] = useState(0);
+
+    const handleRating = (rate: number) => {
+        setRating(rate);
+        if (rate < 5) {
+            alert('The selected rating is less than 5!');
+        }
+    };
+
+    return (
+        <footer className="p-4 bg-gray-800 text-white">
+            <div className="flex space-x-2">
+                {[...Array(5)].map((_, i) => (
+                    <button
+                        onClick={() => handleRating(i + 1)}
+                        className="text-2xl cursor-pointer"
+                        key={i}
+                    >
+                        {i + 1 <= rating ? '★' : '☆'}
+                    </button>
+                ))}
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
