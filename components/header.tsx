7c3f7d8dcbce5a001f2f09020a77f78037b5a59f
Jonas <jonysbina@gmail.com>
2023-11-23 20:02:39 +0100

    Footer in header. Don't ask.

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,38 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+const randomTexts = [
+    'Vyčůrej se do mrazáku',
+    '"To je tak debilní, miluju to!" - Albert Einstein',
+    'Čte to vůbec někdo?',
+    'Pullni ten trigger a bude z tebe n-',
+    'Ahoj, je mi Jarda, jmenuji se jednačtyřicet a jsem youtuber!',
+    'Dex dex dex -',
+    'Věděli jste, že pokud přijdete o obě nohy, nebudete už moct chodit?',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
+    'JAKEJ ŠTEFAN? Však to je Elvis Presley!',
+    'Věděli jste, že jste nevěděli?',
+    'Bagr je v podstatě traktor, který umí bagrovat.',
+    'Nikdo nic neviděl',
+    'Ondřeji prosím!',
+];
+
+const randomIndex = Math.floor(Math.random() * randomTexts.length);
+const randomText = randomTexts[randomIndex];
 const Header = () => {
   return (
+
     <div className='w-1/1 content-center'>
       <Link href="/">
       <a>
         <Image src="/logo.svg" height={400} width={1000} alt="logo" />
         </a>
         </Link>
+        <footer style={{ textAlign: 'center', backgroundColor: 'transparent', color: 'white', padding: '35px', fontSize: '1.6em' }}>
+            {randomText}
+        </footer>
     </div>
     
   )
