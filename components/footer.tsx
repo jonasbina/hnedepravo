c9e23893c2fe8a3286ec96d17ab22c363c19bdfc
Jonas <jonysbina@gmail.com>
2023-11-23 20:58:39 +0100

    Added footer to header. Don't ask

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 const Footer = () => {
     const [rating, setRating] = useState(0);
+    const [hoveredRating, setHoveredRating] = useState(0);
 
     const handleRating = (rate: number) => {
         setRating(rate);
@@ -15,19 +16,21 @@ const Footer = () => {
     return (
         <footer className="p-4 bg-black text-white flex items-center justify-center">
             <div className="flex flex-col items-center text-center">
-                <h1 className="mb-4 text-lg">Please rate our website</h1>
+                <h1 className="mb-4 text-lg">Ohodnoť naši stránku!</h1>
                 <div className="flex space-x-2 mb-4">
                     {[...Array(5)].map((_, i) => (
                         <button
                             onClick={() => handleRating(i + 1)}
+                            onMouseEnter={() => setHoveredRating(i + 1)}
+                            onMouseLeave={() => setHoveredRating(0)}
                             className="text-2xl cursor-pointer hover:text-yellow-500"
                             key={i}
                         >
-                            {i + 1 <= rating ? '★' : '☆'}
+                            {i + 1 <= (hoveredRating || rating) ? '★' : '☆'}
                         </button>
                     ))}
                 </div>
-                <p className="text-sm">Your feedback is important to us</p>
+                <p className="text-sm">Vážíme si Vaší zpětné vazby. Přesně proto žádné hodnocení neukládáme. © Hnědé Právo Media 2023</p>
             </div>
         </footer>
     );
