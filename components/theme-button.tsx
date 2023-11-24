2bf41c753f376027758472409a5515d3ab6a549b
Jonáš Bína <62139134+jonasbina@users.noreply.github.com>
2023-11-24 13:30:39 +0100

    Update theme-button.tsx

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -6,7 +6,7 @@ const ThemeButton = () => {
     return (
         <button
             onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}
-            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'>
+            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg '>
             Toggle Mode
         </button>
     )
