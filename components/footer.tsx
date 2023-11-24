c872196b88e80badf71ac8057e7014c009fc433f
Jonáš Bína <62139134+jonasbina@users.noreply.github.com>
2023-11-24 13:17:21 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -33,6 +33,12 @@ const Footer = () => {
                 <p className="text-sm">Vážíme si Vaší zpětné vazby. Přesně proto žádné hodnocení neukládáme.
                     © Hnědé Právo Media 2023
                 </p>
+
+                <button
+                    onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}
+                    className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'>
+                    Light Mode / Dark mode
+                </button>
             </div>
         </footer>
     );
