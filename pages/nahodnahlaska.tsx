1b0d68844914d4140ec43e044a86cbbe7b28f164
Jonas <jonysbina@gmail.com>
2023-11-27 15:59:10 +0100

    stuff

--- pages/nahodnahlaska.tsx
+++ pages/nahodnahlaska.tsx
@@ -6,52 +6,29 @@ import fs from "fs";
 
 
 const NahodnaHlaska = ({ hlasky }: { hlasky: string[] }) => {
-    const [currentHlaska, setCurrentHlaska] = useState<string>(getRandomHlaska());
+    // const [currentHlaska, setCurrentHlaska] = useState<string>(getRandomHlaska());
 
-    function getRandomHlaska() {
-        const randomIndex = Math.floor(Math.random() * hlasky.length);
-        return getQuotes()[randomIndex];
-    }
-
-    function handleGenerate() {
-        setCurrentHlaska(getRandomHlaska());
-    }
+    // function getRandomHlaska() {
+    //     const randomIndex = Math.floor(Math.random() * hlasky.length);
+    //     return getQuotes()[randomIndex];
+    // }
+    //
+    // function handleGenerate() {
+    //     setCurrentHlaska(getRandomHlaska());
+    // }
 
     return (
         <div>
-            <h1 className="text-3xl font-bold mb-4">Náhodná hláška</h1>
-            <p className="text-lg">{currentHlaska}</p>
+            <h1 className="text-3xl font-bold mb-4">Nevím jak si to našel, každopádně tahle stránka zatím nefunguje :(</h1>
+            <p className="text-lg">{}</p>
             <button
                 className="bg-blue-500 text-white px-4 py-2 mt-4 rounded"
-                onClick={handleGenerate}
+                // onClick={}
             >
-                Generovat novou hlášku
+                Zbytečné tlačítko
             </button>
         </div>
     );
 };
 
 export default NahodnaHlaska;
-const getQuotes = () => {
-    const postsDirectory = path.join(process.cwd(), '_posts');
-    const filenames = fs.readdirSync(postsDirectory);
-    let allQuotes = [];
-
-    filenames.forEach((filename) => {
-        const filePath = path.join(postsDirectory, filename);
-        const fileContents = fs.readFileSync(filePath, 'utf8');
-        const { content } = matter(fileContents);
-
-        const startIndex = content.indexOf('## **Hlášky týdne**');
-        const endIndex = content.indexOf('## **', startIndex + 1);
-        const quotesSection = content.slice(startIndex, endIndex !== -1 ? endIndex : undefined);
-
-        const quoteRegex = /„(.+?)“\s*-\s*(.+)/g;
-        let match;
-        while ((match = quoteRegex.exec(quotesSection)) !== null) {
-            allQuotes.push({ quote: match[1], author: match[2] });
-        }
-    });
-
-    return allQuotes;
-};
