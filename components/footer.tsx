b4d79f46ef6e9b07cf6db13b69abeedea5033374
Jonas <jonysbina@gmail.com>
2023-11-27 13:13:19 +0100

    stuff

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     const handleRating = (rate: number) => {
         setRating(rate);
         if (rate < 5) {
-            alert('Ty negře dej tam 5 hvězd!');
+            alert('Ty černokožní individuum, dej tam 5 hvězd!');
         }
     };
 
