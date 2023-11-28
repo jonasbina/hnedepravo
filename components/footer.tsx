a1c28f9c076fbb6f27c11ba341ba9a914144cbf4
Jonas <jonysbina@gmail.com>
2023-11-28 20:39:44 +0100

    navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -17,8 +17,10 @@ const Footer = () => {
     return (
         <footer className="p-4 bg-black text-white flex items-center justify-center">
             <div className="flex flex-col items-center text-center">
+                <h1 className="mb-4 text-lg">Další části stránky</h1>
                 <a href={"/nahodna-hlaska"}>Generátor náhodných hlášek</a>
                 <a href={"/databaze-hlasek"}>Databáze hlášek</a>
+                <a></a>
                 <h1 className="mb-4 text-lg">Ohodnoť naši stránku!</h1>
                 <div className="flex space-x-2 mb-4">
                     {[...Array(5)].map((_, i) => (
