58f28463f849de875617c2aa0ab1012b7af108d0
Jonas <jonysbina@gmail.com>
2023-11-28 17:14:06 +0100

    stuff

--- pages/nahodnahlaska.tsx
+++ pages/nahodnahlaska.tsx
@@ -15,21 +15,21 @@ const NahodnaHlaska = () => {
 
     return (
         <div className="min-h-screen flex flex-col justify-center items-center">
-            <div className="max-w-md p-4 bg-gray-200 rounded-md">
-                <h1 className="text-2xl font-bold mb-4">Random Quote</h1>
+            <div className="max-w-md p-8 bg-gray-200 rounded-md shadow-lg transition-transform transform hover:scale-105">
+                <h1 className="text-3xl font-bold mb-4 text-blue-600">Hnědé Právo Quotes</h1>
                 <blockquote className="italic text-gray-700">
-                    "{currentQuote.Quote}"
-                    <footer className="text-right text-gray-500">- {currentQuote.Author}</footer>
+                    "{currentQuote.quote}"
+                    <footer className="text-right text-gray-500">- {currentQuote.author}</footer>
                 </blockquote>
                 <button
-                    className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none"
+                    className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none transition-transform transform hover:scale-105"
                     onClick={regenerateQuote}
                 >
-                    Regenerate
+                    Generovat znovu
                 </button>
                 <p className="mt-2">
-                    <a href={`/posts/${currentQuote.Filename.replace('.md', '')}`} className="text-blue-500 hover:underline">
-                        View original post
+                    <a href={`/posts/${currentQuote.fileName.replace('.md', '')}`} className="text-blue-500 hover:underline">
+                        Zobrazit originální díl Hnědého Práva
                     </a>
                 </p>
             </div>
@@ -38,3 +38,4 @@ const NahodnaHlaska = () => {
 };
 
 export default NahodnaHlaska;
+
