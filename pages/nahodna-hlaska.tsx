2131e4609b26d3559e0814aa0c101fb0924132d1
Jonas <jonysbina@gmail.com>
2023-11-28 20:04:25 +0100

    navbar

--- pages/nahodna-hlaska.tsx
+++ pages/nahodna-hlaska.tsx
@@ -17,7 +17,7 @@ const NahodnaHlaska = () => {
         setTimeout(() => {
             setCurrentQuote(getRandomQuote(quotesData));
             setRegenerateAnimation(false);
-        }, 500); // Adjust the duration as needed
+        }, 150); // Adjust the duration as needed
     };
 
     return (
@@ -27,8 +27,8 @@ const NahodnaHlaska = () => {
                 <h1 className="text-4xl font-bold mb-4 text-blue-600">Generátor Hlášek</h1>
 
                 <div
-                    className={`max-w-md p-8 bg-gray-200 rounded-md shadow-lg transition-opacity duration-500 ease-in-out ${
-                        regenerateAnimation ? 'opacity-0' : 'opacity-100 hover:opacity-80'
+                    className={`max-w-md p-8 bg-gray-200 rounded-md shadow-lg transition-transform transform hover:scale-105 ${
+                        regenerateAnimation ? 'animate-fade-in' : ''
                     }`}
                 >
                     <blockquote className="text-xl italic text-gray-700 mb-6">
@@ -37,7 +37,7 @@ const NahodnaHlaska = () => {
                     </blockquote>
 
                     <button
-                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none transition-transform transform hover:scale-105"
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none transition-transform transform hover:scale-110"
                         onClick={regenerateQuote}
                     >
                         Generovat znovu
