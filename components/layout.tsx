bedb66009510a29734d0d62955c95d7e572c95c9
Jonas <jonysbina@gmail.com>
2023-11-28 18:41:44 +0100

    databaze hlasek

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,6 @@
 import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
-import Sidebar from './sidebar'
 import {useState} from "react";
 type Props = {
   preview?: boolean
@@ -9,13 +8,10 @@ type Props = {
 }
 
 const Layout = ({ preview, children }: Props) => {
-    const [showSidebar, setShowSidebar] = useState(false);
     return (
-
     <>
         <Meta />
       <div className="min-h-screen flex">
-          <Sidebar show={showSidebar} setter={setShowSidebar} />
           <main>{children}</main>
         <Footer />
       </div>
