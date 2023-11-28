c9a79d02a8cad9443b946463a5b1086a62fd5cb0
Jonas <jonysbina@gmail.com>
2023-11-28 19:09:44 +0100

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import { useState } from 'react';
+import Link from 'next/link';
 
 export const Navbar = () => {
     const [active, setActive] = useState(false);
@@ -8,41 +8,44 @@ export const Navbar = () => {
     };
 
     return (
-        <>
-            <nav className='flex items-center flex-wrap bg-green-400 p-3 '>
-                <Link href='/'>
-                    <a className='inline-flex items-center p-2 mr-4 '>
-            <span className='text-xl text-white font-bold uppercase tracking-wide'>
-              Hnědé právo
-            </span>
-                    </a>
-                </Link>
-                <button
-                    className='inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
-                    onClick={handleClick}
-                >
-                </button>
-                <div className={`${active ? 'block' : 'hidden'} lg:flex`}>
-                    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-                        <Link href='/'>
-                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
-                                Hlavní stránka
-                            </a>
-                        </Link>
-                        <Link href='/databaze-hlasek'>
-                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
-                                Databáze hlášek
-                            </a>
-                        </Link>
-                        <Link href='/nahodna-hlaska'>
-                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
-                                Generátor náhodných hlášek
-                            </a>
-                        </Link>
-                        {/* Add more links as needed */}
-                    </div>
+        <nav className='flex items-center flex-wrap bg-green-400 p-3 '>
+            <Link href='/'>
+                <a className='inline-flex items-center p-2 mr-4 '>
+                    <span className='text-xl text-white font-bold uppercase tracking-wide'>
+                        Hnědé právo
+                    </span>
+                </a>
+            </Link>
+            <button
+                className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+                onClick={handleClick}
+            >
+                {/* Add your icon here */}
+            </button>
+            <div
+                className={`${
+                    active ? 'block' : 'hidden'
+                } lg:hidden w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+            >
+                <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+                    <Link href='/'>
+                        <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
+                            Hlavní stránka
+                        </a>
+                    </Link>
+                    <Link href='/databaze-hlasek'>
+                        <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                            Databáze hlášek
+                        </a>
+                    </Link>
+                    <Link href='/nahodna-hlaska'>
+                        <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                            Generátor náhodných hlášek
+                        </a>
+                    </Link>
+                    {/* Add more links as needed */}
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     );
 };
