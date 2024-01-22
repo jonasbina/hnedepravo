28447c220d46321c70ec55c2a4ba61ed785354c5
Jonas <jonysbina@gmail.com>
2024-01-22 16:54:29 +0100

    LETS GOOOOOO

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -20,7 +20,7 @@ type Props = {
 
 export default function Post({ post, morePosts, preview }: Props) {
   const router = useRouter()
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`
+  const title = `${post.title}`
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
