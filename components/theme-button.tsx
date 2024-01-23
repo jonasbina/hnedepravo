a4832deeb5625b8f93e852a3833e12cf6d73a684
Jonas <jonysbina@gmail.com>
2024-01-23 22:01:08 +0100

    xdd

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -9,7 +9,7 @@ const ThemeButton = () => {
     return (
         <button
             onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}
-            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg '>
+            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 dark:text-white text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg '>
             <Image src={theme === 'dark' ? '/light-mode.svg' : '/dark-mode.svg'} height={24} width={24} alt="theme" />
         </button>
     )
