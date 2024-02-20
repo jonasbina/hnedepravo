fb0dc453b1835c1bb53a1b8e9becceb9cee4ee1f
Jonas <jonysbina@gmail.com>
2024-02-20 19:48:37 +0100

    jestli to bude fungovat...

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,56 +8,132 @@ import { CMS_NAME } from '../lib/constants'
 import Post from '../interfaces/post'
 import Header from '../components/header'
 import { Analytics } from '@vercel/analytics/react';
+// @ts-ignore
+import { Dialog, Transition } from '@headlessui/react'
+import React from 'react'
 
 type Props = {
-  allPosts: Post[]
+    allPosts: Post[]
 }
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
+    const heroPost = allPosts[0]
+    const morePosts = allPosts.slice(1)
 
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="description" content="Hnědé Právo: Smysluplné zprávy!" />
-          <title>Hnědé Právo</title>
-        </Head>
+    let [isOpen, setIsOpen] = React.useState(true)
 
-        <Header/>
-        <Container>
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-      <Analytics/>
-    </>
-  )
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function handleMouseMove(e: React.MouseEvent<HTMLDivElement>) {
+        const rect = e.target.getBoundingClientRect()
+        const x = e.clientX - rect.left
+        const y = e.clientY - rect.top
+        e.target.style.setProperty('--xPos', `${x}px`)
+        e.target.style.setProperty('--yPos', `${y}px`)
+    }
+
+    return (
+        <>
+            <Layout>
+
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta name="description" content="Hnědé Právo: Smysluplné zprávy!" />
+                    <title>Hnědé Právo</title>
+                </Head>
+
+                <Header/>
+                <Container>
+                    <Transition appear show={isOpen} as={React.Fragment}>
+                        <Dialog
+                            as="div"
+                            className="fixed inset-0 z-10 overflow-y-auto"
+                            onClose={closeModal}
+                        >
+                            <div className="min-h-screen px-4 text-center">
+                                <Transition.Child
+                                    as={React.Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0"
+                                    enterTo="opacity-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100"
+                                    leaveTo="opacity-0"
+                                >
+                                    <Dialog.Overlay className="fixed inset-0" />
+                                </Transition.Child>
+
+                                {/* This element is to trick the browser into centering the modal contents. */}
+                                <span className="inline-block h-screen align-middle" aria-hidden="true">
+              &#8203;
+            </span>
+                                <Transition.Child
+                                    as={React.Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 scale-95"
+                                    enterTo="opacity-100 scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 scale-100"
+                                    leaveTo="opacity-0 scale-95"
+                                >
+                                    <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                                        <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                                            Example Modal
+                                        </Dialog.Title>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-500">
+                                                Use this modal as a way to get user's attention to something.
+                                            </p>
+                                        </div>
+
+                                        <div className="mt-4">
+                                            <button
+                                                type="button"
+                                                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                                                onClick={closeModal}
+                                                onMouseMove={handleMouseMove}
+                                            >
+                                                Close
+                                            </button>
+                                        </div>
+                                    </div>
+                                </Transition.Child>
+                            </div>
+                        </Dialog>
+                    </Transition>
+
+                    {heroPost && (
+                        <HeroPost
+                            title={heroPost.title}
+                            coverImage={heroPost.coverImage}
+                            date={heroPost.date}
+                            author={heroPost.author}
+                            slug={heroPost.slug}
+                            excerpt={heroPost.excerpt}
+                        />
+                    )}
+                    {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+                </Container>
+            </Layout>
+            <Analytics/>
+        </>
+    )
 }
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+    const allPosts = getAllPosts([
+        'title',
+        'date',
+        'slug',
+        'author',
+        'coverImage',
+        'excerpt',
+    ])
 
-  return {
-    props: { allPosts },
-  }
+    return {
+        props: { allPosts },
+    }
 }
